a5c424086c7c1fb034ed7d6028556dfc5822e2bc
Ludwing Rivera A <ludwing.ra@gmail.com>
2018-09-20 16:33:16 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePagesComponent } from './components/home-pages/home-pages.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PrivatePagesComponent } from './components/private-pages/private-pages.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePagesComponent,
+    NavBarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivatePagesComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home-pages/home-pages.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomePagesComponent } from './home-pages.component';
+
+describe('HomePagesComponent', () => {
+  let component: HomePagesComponent;
+  let fixture: ComponentFixture<HomePagesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomePagesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
