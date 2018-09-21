c892246d26c9e0084f3b823ad2822d3f2376d4d7
Ludwing Rivera A <ludwing.ra@gmail.com>
2018-09-20 19:23:55 -0500

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAdtV-wqGNREyXMlwgKz_51EHSeW1QtFi0",
+    authDomain: "loginapp-c7531.firebaseapp.com",
+    databaseURL: "https://loginapp-c7531.firebaseio.com",
+    projectId: "loginapp-c7531",
+    storageBucket: "loginapp-c7531.appspot.com",
+    messagingSenderId: "329813507252"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyAdtV-wqGNREyXMlwgKz_51EHSeW1QtFi0",
+    authDomain: "loginapp-c7531.firebaseapp.com",
+    databaseURL: "https://loginapp-c7531.firebaseio.com",
+    projectId: "loginapp-c7531",
+    storageBucket: "loginapp-c7531.appspot.com",
+    messagingSenderId: "329813507252"
+  }
 };
 
 /*
